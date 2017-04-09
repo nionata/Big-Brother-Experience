8fa428330cc17cf7c3054c3488870461afdef335
Nicholas Ionata <n.ionata129@gmail.com>
2017-04-09 16:13:55 -0400

    Added new images and home page

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import bb from './bb-head.png';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
+        <img src={bb} className="App-logo" alt="logo" />
+        <h2>Welcome to Big Brother Experience</h2>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          To get started, enter your name below.
         </p>
+        <input type="text" ref="inputName" className="form-inline" placeholder="Winston Smith"/>
+        <button className="btn btn-primary">Next</button>
       </div>
     );
   }
