1b71a5f6dfe2a7c5a4fbea7491cdd582e9d647d0
Nicholas Ionata <n.ionata129@gmail.com>
2017-04-13 09:46:27 -0400

    Initial setup

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Router, Route, browserHistory} from 'react-router'
 import App from './assets/components/app.js';
+import About from './assets/components/about.js'
+import NotFound from './assets/components/about.js'
 import './index.css';
 
 ReactDOM.render(
-  <App />,
+  <Router history={browserHistory}>
+    <Route path="/" component={App} />
+    <Route path="/about" component={About} />
+    <Route path='*' component={NotFound} />
+  </Router>,
   document.getElementById('root')
 );
