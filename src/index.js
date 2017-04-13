d4bb7c84cca9537eb6a4ca6bbde26c5642a8fd42
Nicholas Ionata <n.ionata129@gmail.com>
2017-04-13 10:01:49 -0400

    Attempting to get router setup

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Router, Route, browserHistory} from 'react-router'
+import { Router, Route, browserHistory } from 'react-router'
 import App from './assets/components/app.js';
 import About from './assets/components/about.js'
 import NotFound from './assets/components/about.js'
@@ -10,7 +10,7 @@ ReactDOM.render(
   <Router history={browserHistory}>
     <Route path="/" component={App} />
     <Route path="/about" component={About} />
-    <Route path='*' component={NotFound} />
+    <Route path="*" component={NotFound} />
   </Router>,
   document.getElementById('root')
-);
+)
