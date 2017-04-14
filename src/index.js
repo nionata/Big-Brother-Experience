f78b79257a2503bcedd8ba5dbee9777d16a75bfa
Nicholas Ionata <n.ionata129@gmail.com>
2017-04-14 09:55:57 -0400

    Added routing to home, about, and 404

--- src/index.js
+++ src/index.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, browserHistory } from 'react-router'
+import { Switch, Router, Route } from 'react-router'
 import App from './assets/components/app.js';
 import About from './assets/components/about.js'
-import NotFound from './assets/components/about.js'
+import NotFound from './assets/components/404.js'
 import './index.css';
 
+import createBrowserHistory from 'history/createBrowserHistory'
+
+const newHistory = createBrowserHistory();
+
 ReactDOM.render(
-  <Router history={browserHistory}>
-    <Route path="/" component={App} />
-    <Route path="/about" component={About} />
-    <Route path="*" component={NotFound} />
+  <Router history={newHistory}>
+    <div>
+      <Switch>
+        <Route exact path="/" component={App} />
+        <Route exact path="/about" component={About} />
+        <Route component={NotFound} />
+      </Switch>
+    </div>
   </Router>,
   document.getElementById('root')
 )
