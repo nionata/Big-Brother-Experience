4f7bf11d7f6cd43d9ae872475a3cfb6673386528
Nicholas Ionata <n.ionata129@gmail.com>
2017-04-15 22:49:28 -0400

    Finished implementing the resutls

--- src/assets/components/results.js
+++ src/assets/components/results.js
@@ -2,51 +2,6 @@ import React, { Component } from 'react';
 import '../styling/results.css'
 
 class Results extends Component {
-    /*
-    renderMember(item, index) {
-        if(item.id === this.state.editing) {
-            return (
-                <tr key={index}>
-                    <td><input type="text" ref="inputName" className="form-control" placeholder="Name (ex. John Doe)" defaultValue={item.name}/></td>
-                    <td><input type="time" ref="inputAvailTimeStart" className="form-control" defaultValue={item.availTimeStart}/></td>
-                    <td><input type="time" ref="inputAvailTimeEnd" className="form-control" defaultValue={item.availTimeEnd}/></td>
-                </tr>
-            )
-        } else {
-            return (
-                <tr key={index}>
-                    <td>{item.name}</td>
-                    <td>{this.milToStandard(item.availTimeStart)}</td>
-                    <td>{this.milToStandard(item.availTimeEnd)}</td>
-                    {
-                        !this.state.isAddingItem && this.state.editing === null ? (
-                            <td>
-                                <a href=""><span className="glyphicon glyphicon-pencil small" onClick={this.handleOnEdit} data-id={item.id}></span></a>
-                                <a href=""> <span className="glyphicon glyphicon-remove small" onClick={this.deleteMember} data-id={item.id}></span></a>
-                            </td>
-                        ) : (
-                            null
-                        )
-                    }
-                </tr>
-            )
-        }
-    }
-    */
-  renderQuestions(values) {
-      document.createElement("span")
-      for(var i = 0; i < Object.keys(values).length - 1; i++) {
-          return (
-              <tr>
-                <td>{i}</td>
-                <td>{values[i].question}</td>
-                <td>{values[i].answer[0]}</td>
-                <td>{values[i].selected}</td>
-              </tr>
-          )
-      }
-  }
-
   gradeQuestions(values) {
     var grade = 0
 
@@ -69,6 +24,12 @@ class Results extends Component {
   }
 
   render() {
+    var fieldValues = []
+
+    for(var i = 0; i < Object.keys(this.props.fieldValues).length - 1; i++) {
+        fieldValues.push(this.props.fieldValues[i])
+    }
+
     return (
         <div className="container-fluid">
             <h1>{this.props.fieldValues.name}, Big Brother graded your test and you got a {this.gradeQuestions(this.props.fieldValues)}%</h1>
@@ -82,7 +43,35 @@ class Results extends Component {
                         </tr>
                     </thead>
                     <tbody>
-                        {this.renderQuestions(this.props.fieldValues)}
+                        {fieldValues.map(function(item, index) {
+                            var color = ""
+
+                            if(item.choices[item.answer[0]] == item.choices[item.selected]) {
+                                color = "#31dd8a"
+                            } else {
+                                color = "#ee3b3b"
+                            }
+
+                            var style = {
+                                backgroundColor: color
+                            }
+                            return (
+                                <tr key={index} style={style}>
+                                    <td>{index + 1}</td>
+                                    <td>{item.question}</td>
+                                    <td>
+                                        {
+                                            item.answer.map(function(item2, index2) {
+                                                return (
+                                                    item.choices[item2]
+                                                )
+                                            })
+                                        }
+                                    </td>
+                                    <td>{item.choices[item.selected]}</td>
+                                </tr>
+                            )
+                        })}
                     </tbody>
             </table>
         </div>
